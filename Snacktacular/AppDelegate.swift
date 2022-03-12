2221a1d9e26e6d80d927480300aa32207cb42ec7
Song San <songsan2012@gmail.com>
2022-03-12 15:23:35 -0800

    Add location lookup

--- Snacktacular/AppDelegate.swift
+++ Snacktacular/AppDelegate.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import Firebase
+import GooglePlaces
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         
         FirebaseApp.configure()
+        GMSPlacesClient.provideAPIKey(APIKeys.googlePlacesKey)
         
         return true
     }
--- Snacktacular/SpotDetailViewController.swift
+++ Snacktacular/SpotDetailViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import GooglePlaces
 
 class SpotDetailViewController: UIViewController {
 
@@ -61,10 +62,60 @@ class SpotDetailViewController: UIViewController {
     }
     
 
-
     @IBAction func cancelButtonPressed(_ sender: UIBarButtonItem) {
             leaveViewController()
         
     }
     
+    
+
+    @IBAction func locationButtonPressed(_ sender: UIBarButtonItem) {
+        
+        let autocompleteController = GMSAutocompleteViewController()
+                    autocompleteController.delegate = self
+                
+                // Display the autocomplete view controller.
+                    present(autocompleteController, animated: true, completion: nil)
+        
+    }
+    
+    
+}
+
+
+extension SpotDetailViewController: GMSAutocompleteViewControllerDelegate {
+
+  // Handle the user's selection.
+  func viewController(_ viewController: GMSAutocompleteViewController, didAutocompleteWith place: GMSPlace) {
+    print("Place name: \(place.name)")
+    print("Place ID: \(place.placeID)")
+    print("Place attributions: \(place.attributions)")
+    
+      spot.name = place.name ?? "Unknown Place"
+      spot.address = place.formattedAddress ?? "Unknown Address"
+      print("Coordinates = \(place.coordinate)")
+      updateUserInterface()
+      
+      dismiss(animated: true, completion: nil)
+  }
+
+  func viewController(_ viewController: GMSAutocompleteViewController, didFailAutocompleteWithError error: Error) {
+    // TODO: handle the error.
+    print("Error: ", error.localizedDescription)
+  }
+
+  // User canceled the operation.
+  func wasCancelled(_ viewController: GMSAutocompleteViewController) {
+    dismiss(animated: true, completion: nil)
+  }
+
+  // Turn the network activity indicator on and off again.
+  func didRequestAutocompletePredictions(_ viewController: GMSAutocompleteViewController) {
+    UIApplication.shared.isNetworkActivityIndicatorVisible = true
+  }
+
+  func didUpdateAutocompletePredictions(_ viewController: GMSAutocompleteViewController) {
+    UIApplication.shared.isNetworkActivityIndicatorVisible = false
+  }
+
 }
