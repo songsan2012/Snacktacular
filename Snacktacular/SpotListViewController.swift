1b9f5cb95e97ca8c17cd46a6c0857038adfba429
Song San <songsan2012@gmail.com>
2022-03-12 21:38:15 -0800

    Distance for each location and sort by distance working

--- Snacktacular/SpotListViewController.swift
+++ Snacktacular/SpotListViewController.swift
@@ -9,12 +9,12 @@ import UIKit
 import CoreLocation
 
 class SpotListViewController: UIViewController {
-
+    
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var sortSegmentedControl: UISegmentedControl!
     
     
-//    var spots = ["Island Creek Oysters", "El Pelon", "Shake Shack", "Pino's Pizza"]
+    //    var spots = ["Island Creek Oysters", "El Pelon", "Shake Shack", "Pino's Pizza"]
     var spots: Spots!
     var locationManager: CLLocationManager!
     var currentLocation: CLLocation!
@@ -31,13 +31,14 @@ class SpotListViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
+        getLocation()
         spots.loadData {
             self.sortBasedOnSegmentPressed()
             self.tableView.reloadData()
         }
     }
     
-
+    
     func configureSegmentedControl() {
         // set font colors for segmented control
         let orangeFontColor = [NSAttributedString.Key.foregroundColor : UIColor(named: "PrimaryColor") ?? UIColor.orange]
@@ -57,7 +58,7 @@ class SpotListViewController: UIViewController {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "ShowDetail" {
             let destination = segue.destination as! SpotDetailViewController
-            let selectedIndexPath = tableView.indexPathForSelectedRow! 
+            let selectedIndexPath = tableView.indexPathForSelectedRow!
             destination.spot = spots.spotArray[selectedIndexPath.row]
         }
     }
@@ -67,7 +68,7 @@ class SpotListViewController: UIViewController {
         case 0:  // A-Z
             spots.spotArray.sort(by: {$0.name < $1.name})
         case 1: // closest
-            print("TODO: closest in Switch")
+            spots.spotArray.sort(by: {$0.location.distance(from: currentLocation) < $1.location.distance(from: currentLocation)})
         case 2: // averageRating
             print("TODO: averageRating in Switch")
         default:
@@ -94,7 +95,11 @@ extension SpotListViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! SpotTableViewCell
-        cell.nameLabel?.text = spots.spotArray[indexPath.row].name
+        
+        if let currentLocation = currentLocation {
+            cell.currentLocation = currentLocation
+        }
+        cell.spot = spots.spotArray[indexPath.row]
         return cell
         
     }
@@ -105,3 +110,70 @@ extension SpotListViewController: UITableViewDelegate, UITableViewDataSource {
     
     
 }
+
+
+extension SpotListViewController: CLLocationManagerDelegate {
+    
+    func getLocation() {
+        // Creating a CLLocationManager will automatically check authorization
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        print("👮‍♀️👮‍♀️ Checking authentication status.")
+        handleAuthenticalStatus(status: status)
+    }
+    
+    func handleAuthenticalStatus(status: CLAuthorizationStatus){
+        
+        switch status {
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .restricted:
+            self.oneButtonAlert(title: "Location services denied", message: "It may be that parental controls are restricting location use in this app.")
+        case .denied:
+            showAlertToPrivacySettings(title: "User has not authorized location services", message: "Select 'Settings' below to enable device settings and enable location services for this app.")
+        case .authorizedAlways, .authorizedWhenInUse:
+            locationManager.requestLocation()
+        @unknown default:
+            print("😡😡 DEVELOPER ALERT: Unknown case of status in handleAuthenticalStatus \(status)")
+        }
+        
+    }
+    
+    func showAlertToPrivacySettings(title: String, message: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        guard let settingsURL = URL(string: UIApplication.openSettingsURLString) else {
+            print("😡😡 Something went wrong getting the UIApplication.openSettingsURLString")
+            return
+        }
+        let settingsAction = UIAlertAction(title: "Settings", style: .default) { (_) in
+            UIApplication.shared.open(settingsURL, options: [:], completionHandler: nil)
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alertController.addAction(settingsAction)
+        alertController.addAction(cancelAction)
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        // TODO: Deal with change in location
+        
+        
+        currentLocation = locations.last ?? CLLocation()
+        print("🗺 Current location is \(currentLocation.coordinate.latitude),\(currentLocation.coordinate.longitude)")
+        
+        sortBasedOnSegmentPressed()
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        // TODO: Deal with error
+        print("ERROR: \(error.localizedDescription). Failed to get device location.")
+    }
+    
+}
+
+
--- Snacktacular/SpotTableViewCell.swift
+++ Snacktacular/SpotTableViewCell.swift
@@ -6,13 +6,29 @@
 //
 
 import UIKit
+import CoreLocation
 
 class SpotTableViewCell: UITableViewCell {
     @IBOutlet weak var nameLabel: UILabel!
-    
     @IBOutlet weak var distanceLabel: UILabel!
-    
     @IBOutlet weak var ratingLabel: UILabel!
+    
+    var currentLocation: CLLocation!
+    var spot: Spot! {
+        didSet {
+            nameLabel.text = spot.name
+            ratingLabel.text = "Avg. Rating: \(spot.averageRating)"
+            
+            guard let currentLocation = currentLocation else {
+                distanceLabel.text = "Distance: -.-"
+                return
+            }
+            let distanceInMeters = spot.location.distance(from: currentLocation)
+            let distanceInMiles = ((distanceInMeters * 0.00062137) * 10).rounded() / 10
+            distanceLabel.text = "Distance: \(distanceInMiles) miles"
+        }
+    }
+    
     
     override func awakeFromNib() {
         super.awakeFromNib()
