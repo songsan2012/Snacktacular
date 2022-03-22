971d1b203313c228b713836963dcee53da48a638
Song San <songsan2012@gmail.com>
2022-03-21 17:19:54 -0700

    Captures all Users of the Apps now in its own collection.

--- Snacktacular/LoginViewController.swift
+++ Snacktacular/LoginViewController.swift
@@ -38,7 +38,20 @@ class LoginViewController: UIViewController {
             loginViewController.modalPresentationStyle = .fullScreen
             present(loginViewController, animated: true, completion: nil)
         } else { // user is already logged in
-            performSegue(withIdentifier: "FirstShowSegue", sender: nil)
+            guard let currentUser = authUI.auth?.currentUser else {
+                print("😡 ERROR: Couldn't get currentUser")
+                return
+            }
+            let snackUser = SnackUser(user: currentUser)
+            snackUser.saveIfNewUser { (success) in
+                if success {
+                    self.performSegue(withIdentifier: "FirstShowSegue", sender: nil)
+                } else {
+                    print("😡 ERROR: Tried to save a new SnackUser, but failed")
+                }
+            }
+            
+            
         }
     }
     
